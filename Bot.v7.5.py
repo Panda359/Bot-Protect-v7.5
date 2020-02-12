8849b6ba91fbbfe190c4f33da44af538f41e59aa
Panda359 <jiewjaw@gmail.com>
2020-02-12 19:29:26 +0700

    Update Bot.v7.5.py

--- Bot.v7.5.py
+++ Bot.v7.5.py
@@ -9,7 +9,7 @@ from gtts import gTTS
 
 
 cl = LINETCR.LINE()
-cl.login(token="EopxlGUaTxY2kBlfN9A0.1InGNGZn7NNY7fUrZGx9ua.UfOImWIBt/YBH9Tvw3k0vF0OhteAZK0jMGOfjrGNNXY=")
+cl.login(token="fSTr2G4/t3EQ2vocVfa7xSRDUWDJVkKtPYFOTfQOhSE+H4XS9iir2Scv/jjqNAl4OC3fZlgTSHPvEAv0SC49WkhLza2J3qWAWJ5BotlBD1brgoCWAjJT4KGZDNeABUBMHgcn68Ofb+t7srQviEYjOgdB04t89/1O/w1cDnyilFU=")
 cl.loginResult()
 
 kt = LINETCR.LINE()
